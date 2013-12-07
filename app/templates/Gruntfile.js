ac462034b8571cbb54c5469c598fd35769b170c2
Alexandre Mercier <pro.alexandre.mercier@gmail.com>
2013-12-07 22:30:32 +0100

    Removed obsolete lines in Gruntfile

--- app/templates/Gruntfile.js
+++ app/templates/Gruntfile.js
@@ -15,9 +15,6 @@ module.exports = function (grunt) {
     // Time how long tasks take. Can help when optimizing build times
     require('time-grunt')(grunt);
 
-    // Expose the proxy function to use in the connect middleware
-    var proxySnippet = require('grunt-connect-proxy/lib/utils').proxyRequest;
-
     // Define the configuration for all the tasks
     grunt.initConfig({
 
